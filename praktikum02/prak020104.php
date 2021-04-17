5da243f959e3b5e539deb1cbbbd9c0b3035a0054
Salikjpr21 <71271449+Salikjpr21@users.noreply.github.com>
2021-04-17 20:48:45 +0700

    Add files via upload

---
+++ praktikum02/prak020104.php
@@ -0,0 +1,17 @@
+<html>
+ 	<head>Headings</head>
+ 	<body>
+ 	<?php
+        $i=1;
+        while ($i <= 6){
+            if($i % 2 == 0){
+                echo "<h$i style ='color: pink;'>Heading $i</h$i>";
+            } 
+            else{
+                echo "<h".$i.">Heading ".$i."</h".$i.">";
+             }
+             $i = $i + 1;
+        }
+ 	?>
+ 	</body>
+</html>
