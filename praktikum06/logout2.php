4133a32558fcef13e46cab5e9b096f2199f47c5b
Salikjpr21 <71271449+Salikjpr21@users.noreply.github.com>
2021-04-29 21:28:46 +0700

    Delete praktikum06 directory

--- praktikum06/logout2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    // hapus cookie
-    setcookie("namauser", "", -1, "/");
-
-    // redirect ke halaman login
-    header("Location: login.html");
-
-?>
--- praktikum06/tebak angka/ulangi.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "Selamat ya… Anda benar, saya telah memilih bilangan " .$_COOKIE['bilacak'];
-setcookie("bilacak", "", time()-3*30*24*3600,"/");
-echo "<br>";
-echo "<br>";
-echo "<a href='tebakAngka.php'>ulangi lagi</a>";
-echo "<- link untuk mengulangi permainan kembali"
-?>  
